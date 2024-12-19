68fb6f21fae039e62c5c4f7bdf5559df3ac8dafa
Asadullah Shafique <asadullahshafique@hotmail.com>
2024-12-20 01:16:39 +0500

    played

---
+++ .history/next.config_20241220011456.mjs
@@ -0,0 +1,19 @@
+// const nextConfig = {
+//     experimental: {
+//       appDir: true,
+//       turbopack: true, // Enable Turbopack
+//     },
+//   };
+  
+//   module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
+
+// module.exports = {
+//   eslint: {
+//     dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+//   },
+// }
---
+++ .history/next.config_20241220011458.mjs
@@ -0,0 +1,23 @@
+// const nextConfig = {
+//     experimental: {
+//       appDir: true,
+//       turbopack: true, // Enable Turbopack
+//     },
+//   };
+  
+//   module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
+
+// module.exports = {
+//   eslint: {
+//     dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+//   },
+// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
