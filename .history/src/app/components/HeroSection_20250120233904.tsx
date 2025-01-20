e07802812f792004cbe0f3a93da5bda6bbc5950a
Asadullah Shafique <asadullahshafique@hotmail.com>
2025-01-20 23:40:24 +0500

    ok

--- .history/src/app/components/HeroSection_20250120233904.tsx
+++ .history/src/app/components/HeroSection_20250120233904.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const HeroSection = () => {
   return (
-    <section className="bg-[#FBEBB5] min-h-screen flex items-center px-4 md:px-16 grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-4 w-full items-center">
+    <section className="bg-[#FBEBB5] min-h-screen flex items-center px-4 md:px-16 ">
     
       {/* Text Section */}
       <div className="relative w-full sm:w-[440px] h-auto sm:h-[276px] px-4 sm:px-0 text-center sm:text-left">
